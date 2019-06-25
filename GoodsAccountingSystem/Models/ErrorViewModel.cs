9068c3a87567093fdd74272ea7814445b13eca63
silmarillion <mail@mail.com>
2019-06-25 10:47:04 +0300

    Добавьте файлы проекта.

---
+++ GoodsAccountingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoodsAccountingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
