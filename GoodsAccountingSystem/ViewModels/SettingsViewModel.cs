77f1d8af8ba724782bfe0e9e567e91026dfbb2f5
silmarillion <4silmarillion10@gmail.com>
2019-07-17 16:35:54 +0300

    -

---
+++ GoodsAccountingSystem/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodsAccountingSystem.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public int RecordsPerPage { get; set; }
+
+        public SettingsViewModel()
+        {
+
+        }
+    }
+}
