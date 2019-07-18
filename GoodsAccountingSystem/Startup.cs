e05dbc2c92e80633a08956fb03c0062972ff576a
silmarillion <4silmarillion10@gmail.com>
2019-07-18 16:43:50 +0300

    added pagination

--- GoodsAccountingSystem/Startup.cs
+++ GoodsAccountingSystem/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
+using NonFactors.Mvc.Grid;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
